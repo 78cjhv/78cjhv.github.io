427a1a9c645a189f1a959f0e5f204bd5959ba08b
78cjhv <jcvh87@tutanota.com>
2016-03-02 07:55:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   var preprocessedNavRWD = $("#nav").height();
   $("#pres").css("padding-top", preprocessedNavRWD);
 });
-console.log("Interested in seeing how I work? Best pracices and what I recommend? Send me an email for consulting services!");
+console.log("Interested in seeing how I work? Best practices and what I recommend? Send me an email for consulting services!");
 
 /*get some of this http://www.jqueryscript.net/demo/Simple-SPA-Page-Scroller-Plugin-With-jQuery-Scroller/*/
