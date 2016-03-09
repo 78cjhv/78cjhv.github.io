551742cab28466b8129ee4903ce295550c5d4d45
78cjhv <jcvh87@tutanota.com>
2016-03-09 10:32:22 +0800

    Update script.js

--- showcase/script.js
+++ showcase/script.js
@@ -1,7 +1,15 @@
 /*fancy stuff*/
 
 $(document).ready(function () { /*your usual init stuff*/
-  $("button").click(function () {
-    $(".itemEr").fadeToggle(550);
+  
+  $(".tag-WRITING").click(function () {       //  test, this will pretty much be the initial structure     //
+    $(".itemEr").fadeOut(550);                //  until I find an OO or FUNC solution, i know hard coding  //
+    $(".item-WRITING").fadeIn(550);           //  is not elegant or intuitive but i'm rushing a bit since  //
+  });                                         //  i'm always busy working                                  //
+  
+  $(".clear-filter").click(function () {
+    $(".itemEr").fadeIn(550);
+    /*aaaaand reset the target span*/
   });
+  
 });
