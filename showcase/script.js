9ec31032ff2385b92c7e182f2fc8a6b34efeaceb
78cjhv <jcvh87@tutanota.com>
2016-03-10 02:14:39 +0800

    Update script.js

--- showcase/script.js
+++ showcase/script.js
@@ -7,6 +7,41 @@ $(document).ready(function () { /*your usual init stuff*/
     $(".item-WRITING").fadeIn(550);           //  is not elegant or intuitive but i'm rushing a bit since  //
   });                                         //  i'm always busy working                                  //
   
+  $(".tag-DATA").click(function () {
+    $(".itemEr").fadeOut(550);
+    $(".item-DATA").fadeIn(550);
+  }); 
+
+  $(".tag-SEO").click(function () {
+    $(".itemEr").fadeOut(550);
+    $(".item-SEO").fadeIn(550);
+  }); 
+
+  $(".tag-UXUI").click(function () {
+    $(".itemEr").fadeOut(550);
+    $(".item-UXUI").fadeIn(550);
+  }); 
+
+  $(".tag-TRANSLATE").click(function () {
+    $(".itemEr").fadeOut(550);
+    $(".item-TRANSLATE").fadeIn(550);
+  }); 
+
+  $(".tag-EDIT").click(function () {
+    $(".itemEr").fadeOut(550);
+    $(".item-EDIT").fadeIn(550);
+  }); 
+
+  $(".tag-FRONTEND").click(function () {
+    $(".itemEr").fadeOut(550);
+    $(".item-FRONTEND").fadeIn(550);
+  }); 
+
+  $(".tag-BACKEND").click(function () {
+    $(".itemEr").fadeOut(550);
+    $(".item-BACKEND").fadeIn(550);
+  }); 
+  
   $(".clear-filter").click(function () {
     $(".itemEr").fadeIn(550);
     /*aaaaand reset the target span*/
